e7991952e78f905765714607b9d15a02b50839e0
Swager0932 <swagerbot2000@gmail.com>
2024-05-22 14:49:56 -0400

    I didnt commit some changes

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,10 +1,18 @@
+// import font from 'https://fonts.googleapis.com/css?family=Oswald' 
+
 export default function NotFoundPage() {
   return <main id="four04-page">
-  <div id="four04-div" title="404">
+  {/* <div id="four04-div" title="404">
     404
   </div>
   <div id="four04-div" title="This page Doesn't Exist">
     This page Doesn't Exist
-  </div>  
+  </div>   */}
+  <div class="container">
+  <div class="glitch wrap"  data-text=".I S A A C.S A G E S S E. ">.ISAAC.SAGESSE. </div>
+  <div class="glow">Isaac</div>
+  <p class="subtitle">IMPRACTICAL DEVELOPER</p>
+  </div>
+  <div class="scanlines"></div>
 </main>;
 }
